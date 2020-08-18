94fbeefdd27f51c9fc4cea9cb3d576ec7d231dbd
FaustFox <faustffox@gmail.com>
2020-08-18 21:51:44 +0300

    Task_5_2b Done

--- exercises/05_basic_scripts/task_5_2b.py
+++ exercises/05_basic_scripts/task_5_2b.py
@@ -1,3 +1,4 @@
+from sys import argv
 # -*- coding: utf-8 -*-
 """
 Задание 5.2b
@@ -9,3 +10,27 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+#ip, mask = input("Enter network (IP/mask): ").split('/')
+ip, mask = argv[1].split('/')
+oct1, oct2, oct3, oct4 = ip.split('.')
+bin_ip = f"{bin(int(oct1))[2:]:>08}\
+{bin(int(oct2))[2:]:>08}\
+{bin(int(oct3))[2:]:>08}\
+{bin(int(oct4))[2:]:>08}"
+bin_ip = f"{bin_ip[:int(mask)]:<032}"
+bin_mask = f"{('1' * int(mask)):<032}"
+
+print("Network:")
+print(f"{int(bin_ip[:8], 2):<8} \
+{int(bin_ip[8:16], 2):<8} \
+{int(bin_ip[16:24], 2):<8} \
+{int(bin_ip[24:], 2):<8}")
+print(f"{bin_ip[:8]} {bin_ip[8:16]} {bin_ip[16:24]} {bin_ip[24:]}")
+print()
+print("Mask:")
+print('/' + mask)
+print(f"{int(bin_mask[:8], 2):<8} \
+{int(bin_mask[8:16], 2):<8} \
+{int(bin_mask[16:24], 2):<8} \
+{int(bin_mask[24:], 2):<8}")
+print(bin_mask[:8], bin_mask[8:16], bin_mask[16:24], bin_mask[24:])
